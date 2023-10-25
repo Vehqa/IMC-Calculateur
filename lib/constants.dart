0a79585a3b9f17a19117b5b122efd7d36b873a99
Vehqa <pro.nectoux@gmail.com>
2023-10-25 13:44:20 +0200

    Calculate BMI.

--- lib/constants.dart
+++ lib/constants.dart
@@ -13,3 +13,26 @@ const numberTextStyle = TextStyle(
   fontSize: 50.0,
   fontWeight: FontWeight.w900,
 );
+
+const largeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const titleTextStyle = TextStyle(
+  fontSize: 45.0,
+  fontWeight: FontWeight.bold,
+);
+
+const resultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const resultBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const resultPhraseTextStyle = TextStyle(fontSize: 19.0);
